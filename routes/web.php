6b588a7739f26b7b7aaa2d5668dccce49c3c5870
Tyson London <tyson@modcul.com>
2021-01-26 22:48:54 +0100

    added vuejs dist

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('{vue?}', function () {
+    return view('index');
+})->where('vue', '.*');
